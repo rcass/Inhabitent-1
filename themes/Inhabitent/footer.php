fa13a27f1ae20f007ec98e2f4c65fc06ecd9355d
Alex Paydarfar <Alex Paydarfar>
2017-08-12 15:43:21 -0700

    multiple files updated

--- themes/Inhabitent/footer.php
+++ themes/Inhabitent/footer.php
@@ -9,8 +9,19 @@
 
 			</div><!-- #content -->
 
+
 			<footer id="colophon" class="site-footer" role="contentinfo">
+			<h3>contact information</h3>
+			<p><a href="info@inhabitent.com">info@inhabitent.com</a></p>
+			<p>778-456-7891</p>
 				<div class="site-info">
+					<h3>Business Hours</h3>
+					<p>Monday-Friday: 9am to 5pm</p>
+					<p>Saturday: 10am to 2pm</p>
+					<p>Sunday: Closed</p>
+					<div class="copyright">
+						<p>copyright &copy; 2017 Inhabitent</p>
+					</div>
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
