c7aa48c86671c8fa6530fc4f7bf4e6109e347807
Alex Paydarfar <Alex Paydarfar>
2017-08-14 15:19:10 -0700

    about.php updated

---
+++ themes/Inhabitent/page-templates/about.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Template Name: About Page
+*/
+
+
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
+
+<article id="post"-<?php the_ID(); ?>" <?php post_class(); ?>>"
+<header class="entry-header custom-hero">
+  <div class="container">
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+</div>
+</header>
+ <div class="container">
+   <div class="entry-content">
+     <h2>Our Story</h2>
+     <?php echo CFS()->get( 'about_our_story' ); ?>
+
+     <h2>Our Team</h2>
+     <?php echo CFS()->get( 'about_out_team' ); ?>
+</div>
+</div>
+</article>
+</main><!--#main -->
+</div><!--#primary -->
+echo CFS()->get( 'first_name' ); ?>
+<?php get_footer(); ?>
