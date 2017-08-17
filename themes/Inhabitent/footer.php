e21eba07ac754e05d6c134339e449adadabf3800
Alex Paydarfar <Alex Paydarfar>
2017-08-16 20:09:10 -0700

    footer updated

--- themes/Inhabitent/footer.php
+++ themes/Inhabitent/footer.php
@@ -16,64 +16,41 @@
 
 			</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="content-info">
-
-				<div class="site-info site-container">              
-
-					<div id="footer-sidebar" class="secondary">
-						<div id="footer-sidebar1" class="footer-sidebar1">
-							<?php
-							if(is_active_sidebar('footer-sidebar-1')){
-								dynamic_sidebar('footer-sidebar-1');
-							}
-              ?>
-              <div class="contact">
-      
-<h3>Contact INFO</h3>
-<br />
-<p>
-  <i class="fa fa-envelope">
-            </i>
-            <a href="mailto:info@inhabitent.com">
-              info@inhabitent.com</a>
-            </p>
-            <p><i class="fa fa-phone"></i>
-            <a hre="tel:5553434567891"
->778-456-7891></a></p>
-            </div>
-            <div class="business">
-            <h3>Business Hours</h3>
-            <p>Monday-Friday: 9am to 5pm</p>
-            <p>Saturday: 10am to 2pm</p>
-            <p>Sunday: Closed</p>
-
-            </div>
-							<p class="footer-social-media">
-								<span><i class="fa fa-facebook-square" aria-hidden="true"></i></span>
-								<span><i class="fa fa-twitter-square" aria-hidden="true"></i></span>
-								<span><i class="fa fa-google-plus-square" aria-hidden="true"></i></span>
-							</p>
-						</div>
-						<div id="footer-sidebar2" class="footer-sidebar2">
-							<?php
-							if(is_active_sidebar('footer-sidebar-2')){
-								dynamic_sidebar('footer-sidebar-2');
-							}
-							?>
-						</div>
+			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="footer-blocks container">
+				<div class="footer-block-item">
+					<div class="contact">
+						<h3>Contact Info</h3>
+						<p><i class="fa fa-envelope"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
+						<p><i class="fa fa-phone"></i><a href="tel:5553434567891">778-456-7891</a></p>
+						<p>
+							<span><i class="fa fa-facebook-square"></i></span>
+							<span><i class="fa fa-twitter-square"></i></span>
+							<span><i class="fa fa-google-plus-square"></i></span>
+						</p>
 					</div>
-					<div class="footer-logo">
-					<a href="<?php echo home_url() ?>" class="footer-inhabitent-logo"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-text.svg" /> </a>
-					</div>
-					<p class="credit"> Copyright &copy; 2017 Inhabitent</p>
-				</div><!-- .site-info -->
-				<div class="inhabitent">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/inhabitent-logo-text-dark.svg">
 				</div>
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+				<div class="footer-block-item">
+					<div class="business-hours">
+						<h3>Business Hours</h3>
+						<p><span class="day-of-week">Monday-Friday:</span> 9am to 5pm</p>
+						<p><span class="day-of-week">Saturday:</span> 10am to 2pm</p>
+						<p><span class="day-of-week">Sunday:</span> Closed</p>
+					</div>
+				</div>
 
-	</body>
-</html>
+				<div class="footer-block-item">
+					<div class="text-logo">
+						<a href="http://tent.academy.red/" rel="home">
+						<img src="http://tent.academy.red/wp-content/themes/inhabitent/images/inhabitent-logo-text.svg" alt="Inhabitent logo">
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="site-info">
+				<div class="container">
+					Copyright &copy; 2017 Inhabitent</div>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
